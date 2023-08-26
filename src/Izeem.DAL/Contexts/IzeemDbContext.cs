399e7833845f501e70d006aa9b587c136082ca2b
Nurullo <ghostone234qwer.gmail.com>
2023-08-27 01:08:44 +0500

    Add user class with repository

---
+++ src/Izeem.DAL/Contexts/IzeemDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Izeem.DAL.Contexts;
+
+public class IzeemDbContext : DbContext
+{
+    public IzeemDbContext(DbContextOptions<IzeemDbContext> options) : base(options)
+    { }
+}
---
+++ src/Izeem.DAL/IRepositories/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using Izeem.Domain.Commons;
+using System.Linq.Expressions;
+
+namespace Izeem.DAL.IRepositories;
+
+public interface IRepository<TEntity> where TEntity : Auditable
+{
+    Task AddAsync(TEntity entity);
+    void Update(TEntity entity);
+    void Delete(TEntity entity);
+    void Destroy(TEntity entity);
+    Task<TEntity> GetAsync(Expression<Func<TEntity, bool>> expression, string[] includes = null);
+    IQueryable<TEntity> GetAll(Expression<Func<TEntity, bool>> expression = null, bool isNoTracked= true , string[] includes = null);
+    Task SaveAsync();
+}
