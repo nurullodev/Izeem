5ac94d25fd7d9924dfa1074aadcfab3e6df924e5
Nurullo <ghostone234qwer@gmail.com>
2023-09-16 22:42:59 +0500

    Change irepository and implement it's

--- src/Izeem.DAL/IRepositories/IRepository.cs
+++ src/Izeem.DAL/IRepositories/IRepository.cs
@@ -5,11 +5,11 @@ namespace Izeem.DAL.IRepositories;
 
 public interface IRepository<TEntity> where TEntity : Auditable
 {
-    Task AddAsync(TEntity entity);
-    void Update(TEntity entity);
+    ValueTask<TEntity> AddAsync(TEntity entity);
+    TEntity Update(TEntity entity);
     void Delete(TEntity entity);
-    void Destroy(TEntity entity);
-    Task<TEntity> GetAsync(Expression<Func<TEntity, bool>> expression, string[] includes = null);
-    IQueryable<TEntity> GetAll(Expression<Func<TEntity, bool>> expression = null, bool isNoTracked= true , string[] includes = null);
-    Task SaveAsync();
+    ValueTask<bool> DestroyAsync(TEntity entity);
+    ValueTask<TEntity> SelectAsync(Expression<Func<TEntity, bool>> expression, string[] includes = null);
+    IQueryable<TEntity> SelectAll(Expression<Func<TEntity, bool>> expression = null, bool isNoTracked= true , string[] includes = null);
+    ValueTask SaveAsync();
 }
--- src/Izeem.DAL/Repositories/Repository.cs
+++ src/Izeem.DAL/Repositories/Repository.cs
@@ -1,12 +1,43 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Izeem.DAL.IRepositories;
+using Izeem.Domain.Commons;
+using System.Linq.Expressions;
 
-namespace Izeem.DAL.Repositories
+namespace Izeem.DAL.Repositories;
+
+public class Repository<TEntity> : IRepository<TEntity> where TEntity : Auditable
 {
-    internal class Repository
+    public ValueTask<TEntity> AddAsync(TEntity entity)
     {
+        throw new NotImplementedException();
+    }
+
+    public void Delete(TEntity entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public ValueTask<bool> DestroyAsync(TEntity entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public ValueTask SaveAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public IQueryable<TEntity> SelectAll(Expression<Func<TEntity, bool>> expression = null, bool isNoTracked = true, string[] includes = null)
+    {
+        throw new NotImplementedException();
+    }
+
+    public ValueTask<TEntity> SelectAsync(Expression<Func<TEntity, bool>> expression, string[] includes = null)
+    {
+        throw new NotImplementedException();
+    }
+
+    public TEntity Update(TEntity entity)
+    {
+        throw new NotImplementedException();
     }
 }
