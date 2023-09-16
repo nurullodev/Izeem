591cd81ba52392d1039788069310a1f240cecd81
Nurullo <ghostone234qwer@gmail.com>
2023-09-16 22:33:12 +0500

    Add user entity

--- src/Izeem.Domain/Entities/Users/User.cs
+++ src/Izeem.Domain/Entities/Users/User.cs
@@ -1,5 +1,12 @@
-﻿namespace Izeem.Domain.Entities.Users;
+﻿using Izeem.Domain.Commons;
 
-internal class Login
+namespace Izeem.Domain.Entities.Users;
+
+public class User : Auditable
 {
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Phone { get; set; }
+    public string Email { get; set; }
+    public string PasswordHash { get; set; }
 }
