4d406d171209d1b3f675d1432c5612f8f640f4f9
Nurullo <ghostone234qwer@gmail.com>
2023-12-09 14:49:26 +0500

    Fixed

---
+++ src/Izeem.Service/DTOs/Users/UserCreationDto.cs
@@ -0,0 +1,19 @@
+﻿using Izeem.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Izeem.Service.DTOs.Users;
+
+public class UserCreationDto
+{
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public string Phone { get; set; } = string.Empty;
+    public UserRole Role { get; set; } = Role.User;
+}
